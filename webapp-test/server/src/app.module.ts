58994057bc868c225313d06f61a001c06901f790
promitee-k <promiteee@gmail.com>
2024-01-04 05:39:05 +0900

    project migration

---
+++ webapp-test/server/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserModule } from './user/user.module';
+// import { MurmurModule } from './murmur/murmur.module';
+// import { User } from './user/entities/user.entity';
+// import { Murmur } from './murmur/entities/murmur.entity';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'docker',
+    password: 'docker',
+    database: 'test',
+    entities: [],
+    synchronize: true,
+  }) ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
