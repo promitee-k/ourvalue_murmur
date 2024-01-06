ffd7b62519af7aa32dc44e569a297ab9f900c508
promitee-k <promiteee@gmail.com>
2024-01-06 15:22:51 +0900

    initial setup

---
+++ webapp-test/client/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Display the data obtained from API here</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  async asyncData({ $axios }: { $axios: any }): Promise<object> {
+    const res = await $axios.$post('/api/postTest')
+    console.log(res)
+    return {
+      data: res,
+    }
+  },
+  data() {
+    return {}
+  },
+}
+</script>
